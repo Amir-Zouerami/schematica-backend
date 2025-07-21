f12cf10002833f68468e5cc0b0b92e9d22b059ec
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2025-07-21 18:13:29 +0330

    🔬 ESLINT: updated the eslint config with better styles.

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,25 @@
 const js = require('@eslint/js');
 const globals = require('globals');
+const stylistic = require('@stylistic/eslint-plugin');
 
 module.exports = [
 	{
+		ignores: ['dist', 'public', 'app_data'],
+	},
+	{
+		...js.configs.recommended,
 		files: ['**/*.js'],
 		languageOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
+			ecmaVersion: 2020,
+			sourceType: 'commonjs',
+			globals: globals.node,
+		},
+		plugins: {
+			'@stylistic': stylistic,
 		},
 		rules: {
-			'no-unused-vars': 'warn',
-			'no-undef': 'error',
-
-			eqeqeq: ['error', 'always'],
-			curly: 'error',
-			'no-var': 'error',
-			'prefer-const': 'error',
-
-			'no-alert': 'error',
-			'no-eval': 'error',
-			'no-implicit-globals': 'error',
+			'@stylistic/brace-style': ['error', 'stroustrup', { allowSingleLine: false }],
+			'@stylistic/indent': ['error', 'tab', { SwitchCase: 1, MemberExpression: 0 }],
 		},
 	},
-	js.configs.recommended,
 ];
